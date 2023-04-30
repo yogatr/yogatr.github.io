b3b8f738600e83411a70c938146125032b29d8f2
yogatri00 <yogatrinr@gmail.com>
2023-04-30 13:32:50 +0700

    fixed the image problem

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ const sliderMain = new Swiper(".slider_main", {
   parallax: true,
   breakpoints: {
     0: {
-      slidePerView: 2.5,
+      slidesPerView: 2.5,
       spaceBetween: 20,
     },
     680: {
-      slidePerView: 3.5,
+      slidesPerView: 3.5,
       spaceBetween: 60,
     },
   },
